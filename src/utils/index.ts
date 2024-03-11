1b2e59a750719f49eed7dac499f7a5d7ec4849e1
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-11 09:55:25 +0000

    updated api url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-// export const api_url = "https://intellisenseai.fly.dev"
-export const api_url = "http://127.0.0.1:8000"
+export const api_url = "https://intellisenseai.fly.dev"
+// export const api_url = "http://127.0.0.1:8000"
