5f92ed83c1f7795db8157acbaa0a701b02d03517
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-15 23:20:47 +0000

    adding some styling

--- src/app/slide/components/ChatArea.tsx
+++ src/app/slide/components/ChatArea.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 const ChatArea = () => {
   return (
-    <div className='h-full p-5 rounded-md w-1/2 border'>
+    <div className='rounded-md w-1/2 border my-0'>
+        
         <header className='flex justify-between items-center'>
             <p>Chat</p>
             <div>Tools</div>
         </header>
-        <div className='p-5 w-full flex flex-col'>
-            <div className="w-full">
-                <p className="flex">Chat Area</p>
+
+        <div className='p-2 flex-1 flex-col justify-between h-full'>
+            
+            <div className="flex justify-center items-center bg-slate-200 h-[80%]">
+                <p className="">Chat Area</p>
             </div>
-            <div className='flex w-full'>
+
+            <div className='flex'>
                 <input type="text" placeholder='Ask any question...' className='border border-blue-700 px-3 py-1.5 rounded-s-md
-                outline-none' />
+                outline-none w-full' />
                 <button className='bg-blue-700 px-3 py-1.5 rounded-e-md text-white'>Send</button>
             </div>
         </div>
