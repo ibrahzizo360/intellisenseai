97271c2907fd5c1e72d29fe5ca3cfb37c59c8989
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-15 22:52:44 +0000

    adding elements

--- src/app/slide/components/ChatArea.tsx
+++ src/app/slide/components/ChatArea.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const ChatArea = () => {
   return (
-    <div className='h-[90vh] p-5 rounded-md w-1/2 border'>ChatArea</div>
+    <div className='h-full p-5 rounded-md w-1/2 border'>
+        <header className='flex justify-between items-center'>
+            <p>Chat</p>
+            <div>Tools</div>
+        </header>
+        <div>
+            <div className="w-full"></div>
+            <div>
+                <input type="text" />
+                <button></button>
+            </div>
+        </div>
+    </div>
   )
 }
 
