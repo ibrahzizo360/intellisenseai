7cadbf0b8f18b91d9a6ecdcab32487f9a17daed6
ibrahzizo360 <iaziz@st.ug.edu.gh>
2024-03-15 22:40:04 +0000

    added borders

--- src/app/slide/components/ChatArea.tsx
+++ src/app/slide/components/ChatArea.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChatArea = () => {
   return (
-    <div className='h-[90vh] p-5 rounded-md w-1/2'>ChatArea</div>
+    <div className='h-[90vh] p-5 rounded-md w-1/2 border'>ChatArea</div>
   )
 }
 
--- src/app/slide/components/SlideViewer.tsx
+++ src/app/slide/components/SlideViewer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SlideViewer = () => {
   return (
-    <div className='h-[90vh] p-5 rounded-md w-1/2'>SlideViewer</div>
+    <div className='h-[90vh] p-5 rounded-md w-1/2 border'>SlideViewer</div>
   )
 }
 
