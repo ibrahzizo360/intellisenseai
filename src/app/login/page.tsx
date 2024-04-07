673fe5a452eacb7dbb18a7b14a9e27eb82e8aec4
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-04-07 00:24:35 +0000

    done with auth screens

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client' 
 import ButtonLoader from '@/components/loaders/button-loader';
 import Axios from '@/utils/axios';
+import Image from 'next/image';
 import {useState} from 'react'
 import { NotificationManager } from 'react-notifications';
 
@@ -36,30 +37,42 @@ const Login:React.FC = () => {
     }
 
     return (
-        <main className="h-screen flex mx-auto items-center justify-center">
-            <form method="POST">
-                <div className='space-y-4 flex flex-col items-center'>
-                    <h1 className="text-center text-xl font-bold mb-10">Login to your KnowtifAI account</h1>
+        <main className="h-screen flex">
+            <Image src='/login.svg' className='h-screen w-[40%]' height={0} width={0} sizes='100vh' alt='logo' />
 
-                    <input className="px-3 py-1.5 rounded-md outline-none border border-gray-300 w-full" placeholder="email" type='text'
+            <div className="w-full bg-[#DBEEED]">
+            <form method="POST" onSubmit={handleSubmit} className='mx-auto w-[60%]'>
+                <div className='flex flex-col items-center my-24'>
+                
+                <Image src='/logo-round.svg' height={60} width={60} alt='logo' className='' />  
+                    
+                <h1 className="text-center text-2xl font-bold mb-10 mt-16 text-gray-600 tracking-wide">Welcome Back!</h1>
+
+                <input className="px-12 py-5 rounded-3xl  outline-none border border-green-500 w-full mb-6" placeholder="Email address" type='email'
                     value={email} onChange={(e)=> {
                         setEmail(e.target.value)
                     }}/>
-                    <input className="px-3 py-1.5 rounded-md outline-none border border-gray-300 w-full" placeholder="password" type='password'
+                    <input className="px-12 py-5 rounded-3xl  outline-none border border-green-500 w-full mb-3" placeholder="Password" type='password'
                     value={password} onChange={(e)=> {
                         setPassword(e.target.value)
                     }}/>
 
-                    <div>
-                        <p className='text-sm'>Don&apos;t have an account? <a href='/register' className='text-blue-400'>Register</a></p>
+                    <div className='text-left w-full pl-10 mb-8'>
+                        <a href='/forgot-password' className='text-green-500'>Forgot Password?</a>
                     </div>
 
-                    <button type='submit' className='rounded-md px-2 py-1.5 bg-blue-400 text-white w-full hover:bg-blue-200' disabled={loading} onClick={handleSubmit}>
-                        {loading ? <ButtonLoader/> : 'Login'}
+                    
+                    <button type='submit' className='mt-7 rounded-3xl px-2 py-5 bg-[#14a760] hover:bg-[#3f9c6f] text-white w-full' disabled={loading}>
+                        {loading ? <ButtonLoader/> : 'Sign In'}
                     </button>
+
+                    <div className='mt-4'>
+                        <p className='text-sm'>Don&apos;t have an account? <a href='/register' className='text-green-500 ml-3'>Sign Up</a></p>
+                    </div>
                 </div>
             </form>
-        </main>
+            </div>
+        </main> 
     )
 }
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,7 @@
 'use client' 
 import ButtonLoader from '@/components/loaders/button-loader';
 import Axios from '@/utils/axios';
+import Image from 'next/image';
 import {useState} from 'react'
 import { NotificationManager } from 'react-notifications';
 
@@ -39,31 +40,41 @@ const Register:React.FC = () => {
     }
 
     return (
-        <main className="h-screen flex mx-auto items-center justify-center">
-            <form method="POST" onSubmit={handleSubmit}>
-                <div className='space-y-4 flex flex-col items-center'>
-                <h1 className="text-center text-xl font-bold mb-10">Register an account with us</h1>
-                <input className="px-3 py-1.5 rounded-md  outline-none border border-gray-300 w-full" placeholder="Enter email" type='email'
+        <main className="h-screen flex">
+            <Image src='/register.svg' className='h-screen w-[40%]' height={0} width={0} sizes='100vh' alt='logo' />
+
+            <div className="w-full bg-[#DBEEED]">
+            <form method="POST" onSubmit={handleSubmit} className='mx-auto w-[60%]'>
+                <div className='flex flex-col items-center my-24'>
+                
+                <Image src='/logo-round.svg' height={60} width={60} alt='logo' className='' />  
+                    
+                <h1 className="text-center text-2xl font-bold mb-10 mt-16 text-gray-600">Register an account with us</h1>
+
+                <input className="px-12 py-5 rounded-3xl  outline-none border border-green-500 w-full mb-6" placeholder="Email address" type='email'
                     value={email} onChange={(e)=> {
                         setEmail(e.target.value)
                     }}/>
-                    <input className="px-3 py-1.5 rounded-md  outline-none border border-gray-300 w-full" placeholder="username" type='text'
+                    <input className="px-12 py-5 rounded-3xl  outline-none border border-green-500 w-full mb-6" placeholder="Username" type='text'
                     value={username} onChange={(e)=> {
                         setUsername(e.target.value)
                     }}/>
-                    <input className="px-3 py-1.5 rounded-md  outline-none border border-gray-300 w-full" placeholder="password" type='password'
+                    <input className="px-12 py-5 rounded-3xl  outline-none border border-green-500 w-full mb-6" placeholder="password" type='password'
                     value={password} onChange={(e)=> {
                         setPassword(e.target.value)
                     }}/>
 
-                    <div>
-                        <p className='text-sm'>Already have an account? <a href='/login' className='text-blue-400'>Login</a></p>
-                    </div>
-                    <button type='submit' className='rounded-md px-2 py-1.5 bg-blue-400 text-white w-full hover:bg-blue-200' disabled={loading}>
+                    
+                    <button type='submit' className='mt-7 rounded-3xl px-2 py-5 bg-[#14a760] hover:bg-[#3f9c6f] text-white w-full' disabled={loading}>
                         {loading ? <ButtonLoader/> : 'Register'}
                     </button>
+
+                    <div className='mt-4'>
+                        <p className='text-sm'>Already have an account? <a href='/login' className='text-green-500 ml-3'>Login</a></p>
+                    </div>
                 </div>
             </form>
+            </div>
         </main>
     )
 }
