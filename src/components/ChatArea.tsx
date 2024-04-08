66b554d0602d2a4240de4840c94a410ba7789717
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-04-08 02:10:04 +0000

    done with video interaction chat side

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -1,7 +1,16 @@
-import React, { useState, useRef, useEffect  } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ChatLoader from './loaders/ChatLoader';
 import axios from 'axios';
 import Axios from '@/utils/axios';
+import { FiSend } from 'react-icons/fi';
+import { Jura } from 'next/font/google';
+import Image from 'next/image';
+
+const jura = Jura({
+  weight: "500",
+  subsets: ["latin"],
+}
+)
 
 interface ChatProps {
   transcript: string;
@@ -12,7 +21,7 @@ interface Message {
   user: 'user' | 'bot';
 }
 
-const ChatArea: React.FC<ChatProps> = ({transcript}) => {
+const ChatArea: React.FC<ChatProps> = ({ transcript }) => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>('');
   const chatContainerRef = useRef<HTMLDivElement>(null);
@@ -28,11 +37,12 @@ const ChatArea: React.FC<ChatProps> = ({transcript}) => {
     if (loaderRef.current) {
       loaderRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
     }
-  }, [messages,loading]);
+  }, [messages, loading]);
 
-  const sendMessage = async () => {
+  const sendMessage = async (e:any) => {
     setMessages(prevMessages => [...prevMessages, { text: input, user: 'user' }]);
     setLoading(true);
+    setInput('');
     try {
       const response = await fetchMessage(input);
       setMessages(prevMessages => [...prevMessages, { text: response.text, user: 'bot' }]);
@@ -42,16 +52,15 @@ const ChatArea: React.FC<ChatProps> = ({transcript}) => {
     } finally {
       setLoading(false);
     }
-    setInput('');
   };
-  
-  
+
+
   const fetchMessage = async (message: string): Promise<Message> => {
     setLoading(true);
     try {
       const response = await Axios.post(`chat`, { question: message, transcript_text: transcript });
       if (response.status === 200) {
-        console.log(response.data);     
+        console.log(response.data);
         return { text: response.data.chat_completion.content, user: 'bot' };
       } else {
         throw new Error('Invalid response status');
@@ -97,43 +106,45 @@ const ChatArea: React.FC<ChatProps> = ({transcript}) => {
   }
 
   return (
-    <div className='lg:max-h-[88vh] bg-gray-800 rounded-md w-full'>
-     <div className='h-[80%] overflow-y-auto no-scrollbar'>
-      {messages.map((message, index) => (
-        <div key={index} className={`flex ${message.user === 'user' ? '' : ''} text-white my-5 ml-1`} ref={chatContainerRef}>
-          {message.user !== 'user' && <img src="bot.png" alt="User" className="w-7 h-7  mx-2" />}
-          {message.user === 'user' && <img src="user.png" alt="User" className="w-7 h-7  mx-2" />}
-          <div className='w-[92%]'>
-          {message.text.split('\n').map((line, index) => (
-            <div key={index}>
-              {line === "%new%" ? <div className='my-2'/> : line}  
+    <div className='lg:max-h-[85vh] bg-[#dceeed] rounded-md w-full'>
+      <div className='h-[85%] overflow-y-auto no-scrollbar'>
+        {messages.map((message, index) => (
+          <div key={index} className={`flex ${message.user === 'user' ? 'justify-end' : ''} my-5 ml-1`} ref={chatContainerRef}>
+            {message.user !== 'user' && <Image src="logo-round.svg" height={28} width={28} alt="User" className="w-7 h-7  mx-2" />}
+            {/* {message.user === 'user' && <img src="user.png" alt="User" className="w-7 h-7  mx-2" />} */}
+            <div className={`max-w-[90%] bg-white p-2 rounded-b-lg  ${message.user === 'user' ? 'rounded-s-lg mr-3 ' : ' rounded-e-lg mt-4'}`}>
+              {message.text.split('\n').map((line, index) => (
+                <div key={index}>
+                  {line === "%new%" ? <div className='my-2' /> : line}
+                </div>
+              ))}
             </div>
-          ))}
           </div>
-        </div>
-      ))}
-      {loading && <ChatLoader ref={loaderRef}/>} 
-    </div>
+        ))}
+        {loading && <ChatLoader ref={loaderRef} />}
+      </div>
 
-      <hr className='my-1' />
-      <div className='h-[20%] p-3'>
-        <div className='border border-gray-200 rounded-md p-2 space-y-1'>
+      <div className='h-[15%] p-3'>
+        <div className='h-[50px] bg-white rounded-full'>
+          <form className='' onSubmit={(e) => { e.preventDefault(); sendMessage(e); }}>  
           <input
             type='text'
-            className='w-full p-2 bg-gray-800 border-none focus:outline-none text-white'
+            className='w-[85%] ml-8 p-2 pb-2 bg-white border-none rounded-full focus:outline-none'
             placeholder='Type a message...'
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <div className='flex justify-between items-center'>
+          <div className='flex justify-between items-center relative'>
             <div className='flex gap-2 items-center'>
-              <button className='bg-gray-500 text-white rounded-md px-3 py-2 text-xs hover:bg-gray-300' onClick={getQuizzes}>Quiz me</button>
-              <button className='bg-gray-500 text-white rounded-md px-3 py-2 text-xs hover:bg-gray-300' onClick={getSummary}>Summarize</button>
+              <div className={`bg-[#d9d9d9] cursor-pointer  rounded-full px-5 py-1 text-xs hover:bg-gray-300 absolute -bottom-5 left-36 ${jura.className}`} onClick={getQuizzes} >Quiz me</div>
+              <div className={`bg-[#d9d9d9] cursor-pointer font-semibold  rounded-full px-5 py-1 text-xs hover:bg-gray-300 absolute -bottom-5 left-8 ${jura.className} `} onClick={getSummary}>Summarize</div>
             </div>
-            <button className='bg-blue-500 text-white rounded-md px-3 py-2 text-xs' onClick={sendMessage}>
-              Send
+            <button className={`bg-gray-900 text-white rounded-full h-10 w-10 p-1.5 text-xs absolute -bottom-1 right-1 flex justify-center items-center cursor-pointer hover:bg-slate-700`} onSubmit={sendMessage} type='submit'>
+              <FiSend className='h-5 w-5' />
             </button>
+
           </div>
+          </form> 
         </div>
       </div>
     </div>
--- src/components/loaders/ChatLoader.tsx
+++ src/components/loaders/ChatLoader.tsx
@@ -1,13 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ChatLoader = React.forwardRef<HTMLDivElement>(({}, ref) => {
   return (
-    <div ref={ref} className="flex justify-center items-center w-10 h-10 ml-2">
-      <svg className="animate-spin h-10 w-10 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 4.418 3.582 8 8 8v-4zm14-1.709A7.962 7.962 0 0120 12h4c0 4.418-3.582 8-8 8v-4z"></path>
-      </svg>
-    </div>
+    // <div ref={ref} className="flex justify-center items-center w-10 h-10 ml-2">
+    //   <svg className="animate-spin h-10 w-10 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    //     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+    //     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 4.418 3.582 8 8 8v-4zm14-1.709A7.962 7.962 0 0120 12h4c0 4.418-3.582 8-8 8v-4z"></path>
+    //   </svg>
+    // </div>
+
+    <div className='ml-4' ref={ref}>
+      <Image src="logo-round.svg" height={38} width={38} alt="User" className=" animate-spin" />
+    </div> 
   );
 });
 
