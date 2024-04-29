8209031d55b82e22b0c82e4ba73ca0395b4c1e67
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-04-29 10:02:56 +0000

    fix: fixed wrong api url

--- src/app/(portal)/slide/components/ChatArea.tsx
+++ src/app/(portal)/slide/components/ChatArea.tsx
@@ -19,7 +19,8 @@ const ChatArea = ({ messages, setMessages,scrollToPage }: { messages: Message[],
 
 
   const streamResponse = async (input: string, messageId: number) => {
-    const url = `http://127.0.0.1:8000/v2/get_answers`;
+    const base_url = process.env.NEXT_PUBLIC_API_URL; 
+    const url = `${base_url}v2/get_answers`;
     const res = await fetch(url,{
       body: JSON.stringify({ question: input }),
       method: 'POST',
