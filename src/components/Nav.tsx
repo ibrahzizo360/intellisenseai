0ee2a3e71f133f336ea38eef82beb373c3c6ad40
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2024-05-08 21:32:42 +0000

    added drop down to nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,10 +4,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import { IoMdArrowDropdown } from 'react-icons/io'
 
 const Nav = () => {
   const path = usePathname();
   const [user, setUser] = useState(null);
+  const [openFeatures, setOpenFeatures] = useState(false);
   
 
   useEffect(() => {
@@ -36,8 +38,38 @@ const Nav = () => {
         /> 
       </Link>
 
-        <div className='flex gap-16 items-center'>
-            <Link href='/features'><h2 className={`text-sm font-semibold cursor-pointer hover:text-green-400 ${path === '/features' ? 'border-b-2' : ''}`}>Features</h2></Link>
+        <div className='flex gap-16 items-center relative'>
+            
+              <div className="flex items-center gap-2">
+              <Link href='/features'>
+              <h2 className={`text-sm font-semibold cursor-pointer hover:text-green-400 ${path === '/features' ? 'border-b-2' : ''}`}>Features</h2>
+              </Link>
+              <IoMdArrowDropdown className='mt-0.5 h-6 w-6 cursor-pointer' onClick={() => setOpenFeatures(true)} />
+              </div>
+
+              {
+                openFeatures && (
+                  <div className="absolute bg-white rounded-lg top-12 left-5">
+                  <Link href='/video'>
+                    <div className="flex items-center gap-4 p-2 cursor-pointer hover:bg-gray-100 rounded-lg">
+                      <Image src={'Youtube.svg'} alt='' height={20} width={20} />
+                      <p className='text-black text-center'>Video Interaction</p>
+                    </div>
+                  </Link>
+                    <hr />
+                  <Link href='/slide'>
+                    <div className="flex items-center gap-4 p-2 cursor-pointer hover:bg-gray-200">
+                      <Image src={'Document.svg'} alt='' height={20} width={20} />
+                      <p className='text-black'>Document Interaction</p>
+                    </div>
+                  </Link>
+                  </div>
+                )
+              }
+
+
+              
+              
             <Link href='/more'><h2 className={`text-sm font-semibold cursor-pointer hover:text-green-400 ${path === '/more' ? 'border-b-2' : ''}`}>Blog</h2></Link>
             <Link href='/about'><h2 className={`text-sm font-semibold cursor-pointer hover:text-green-400 ${path === '/about' ? 'border-b-2' : ''}`}>About</h2></Link>
             {!user && <Link href='/login'><h2 className={`text-sm font-semibold cursor-pointer hover:text-green-400 ${path === '/login' ? 'underline' : ''}`}>Sign In</h2></Link>}
